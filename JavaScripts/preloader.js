d3b03db2399668258fab95f8bde4511543344882
Ilya <ilyashirobokih@gmail.com>
2023-02-05 01:51:48 +0700

    Initial commit

---
+++ JavaScripts/preloader.js
@@ -0,0 +1,14 @@
+function loadData() {
+    return new Promise((resolve, reject) => {
+        // setTimeout не является частью решения
+        // Код ниже должен быть заменен на логику подходящую для решения вашей задачи
+        setTimeout(resolve, 999);
+    })
+}
+
+loadData()
+    .then(() => {
+        let preloaderEl = document.getElementById('preloader');
+        preloaderEl.classList.add('hidden');
+        preloaderEl.classList.remove('visible');
+    });
